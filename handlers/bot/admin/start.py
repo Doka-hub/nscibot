5b3e245ff2e665749e5d8d718d9d9a4ad964191f
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2021-12-22 16:39:17 +0600

    partner started, page-list ended (90%)

--- handlers/bot/admin/start.py
+++ handlers/bot/admin/start.py
@@ -1,15 +1,8 @@
-import os
-
-from uuid import uuid4
-
-import pandas as pd
-
 from aiogram import types
 
 # local imports
-from keyboards.bot.inline.user import get_menu_inline_keyboard
+from keyboards.bot.inline.admin.menu import get_menu_inline_keyboard
 
-from utils.bot.db_api.user import get_user_list
 from utils.bot import set_commands
 
 from loader import bot_i18n_gettext as _
